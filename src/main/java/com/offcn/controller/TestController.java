9542434b68e8047d89d14b143179a5d944be06cd
xsj <473423451@qq.com>
2020-06-03 15:13:58 +0800

    右侧idea提交的文件

--- src/main/java/com/offcn/controller/TestController.java
+++ src/main/java/com/offcn/controller/TestController.java
@@ -3,8 +3,7 @@ package com.offcn.controller;
 public class TestController {
     public static void main(String[] args) {
         System.out.println("这是右侧的打印测试。。。");
-    }
-    public static void main(String[] args) {
+
         System.out.println("这是左侧的代码。。。");
     }
 }
